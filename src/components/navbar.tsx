04e8fa76920a74555ace43f839e925267fd6fcc5
Mojtaba Amiri <Mojtaba.a.amiri@outlook>
2023-10-05 15:33:36 +0200

    Search Bar added
    Co-authored-by: Fredrik Säfström <Naiso2022@users.noreply.github.com>
    Co-authored-by: Richard Ejdling <chasacademy-richard-ejdling@users.noreply.github.com>
    Co-authored-by: Farida Ouro Möllah <Farida1084@users.noreply.github.com>

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,14 +5,14 @@ export default function Navbar() {
   return (
     <header>
       <nav className=" sticky left-0 top-0 flex w-full bg-sky-400 px-2 py-4">
-        <ul className="flex w-full items-center justify-between">
-          <li className="md:hidden">
+        <ul className="flex w-full items-center">
+          <li className="grow md:hidden">
             <HamburgerMenu />
           </li>
-          <li>
+          <li className="grow">
             <h1>MOVIESHOP</h1>
           </li>
-          <li>
+          <li className="grow">
             <SlBasketLoaded />
           </li>
         </ul>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
+import SearchBar from "~/components/searchbar";
 
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  /* const hello = api.example.hello.useQuery({ text: "from tRPC" });
+   */
   return (
     <div>
+      <SearchBar />
       <p>hej</p>
       <Link href={"./test"}>Go to test</Link>
     </div>
