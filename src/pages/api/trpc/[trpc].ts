2ac76cca00c57cfe516d802a497b4ea24560d119
Mojtaba Amiri <mojtaba.a.amiri@outlook.com>
2025-08-20 12:39:06 +0200

    chore: update Next.js and ESLint configurations, and refactor tRPC API handler for improved type safety

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -2,9 +2,10 @@ import { createNextApiHandler } from "@trpc/server/adapters/next";
 import { env } from "~/env.js";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 // export API handler
-export default createNextApiHandler({
+const handler = createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
   onError:
@@ -16,3 +17,8 @@ export default createNextApiHandler({
         }
       : undefined,
 });
+
+export default handler as (
+  req: NextApiRequest,
+  res: NextApiResponse,
+) => Promise<void>;
